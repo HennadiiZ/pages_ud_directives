c5a46436630f2ea7555fa26e4d77a9e4d5fb5b18
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-05-27 16:08:18 +0300

    Accessing Elements from a Custom Directive

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,12 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
 
-  constructor() { 
+  constructor(private element: ElementRef) { 
     console.log('Class directive used!')
+    console.log(this.element)
+    console.log(this.element.nativeElement)
+
+    // this.element.nativeElement.style.backgroundColor = 'black';
+    this.element.nativeElement.style.border = '2px solid black';
   }
 
 }
