6b884db84548469db8b313bf99ab9f3fee576d32
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-05-27 17:20:46 +0300

    Replacing NgClass

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -7,8 +7,15 @@ export class ClassDirective {
 
   constructor(private element: ElementRef){}
 
-  @Input("appClass") set backgroundColor(color: string) {
-    this.element.nativeElement.style.backgroundColor = color;
+  @Input("appClass") set className(classObj: any) {
+    //this.element.nativeElement.style.backgroundColor = classObj;
+    for(let key in classObj){
+      if(classObj[key]){
+        this.element.nativeElement.classList.add(key);
+      }else{
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
 }
 
